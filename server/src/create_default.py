5e35297c30ef22b7ad1eac210a529ed3b2923b7a
Jacobo Tarragon <jacobo.tarragon@gmail.com>
2016-09-12 17:06:58 +0200

    remove test script

--- server/src/create_default.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from game.modes import DefaultGameMode
-
-tomas = User.objects.get(username="tomas")
-players = User.objects.all()
-
-game = DefaultGameMode.create(owner=tomas, players=players[:9])
